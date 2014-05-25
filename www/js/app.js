eca9ce80b94be7ae2b7b0528f95159fe73d8c5ae
Mehdi Ali <mmehdi@blackberry.com>
2014-05-25 01:10:26 -0400

    feature: added angularJS in index.html, and implemented a basic weather search

---
+++ www/js/app.js
@@ -0,0 +1,15 @@
+angular.module("app", [] )
+        .controller("locationCtrl", function ($scope, $http) {
+            $scope.myData = {};
+            $scope.myData.doClick = function(item, event) {
+                var baseUrl = "http://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+                var responsePromise = $http.get(baseUrl+$scope.location);
+                responsePromise.success(function(data, status, headers, config) {
+                    $scope.myData.data = data;
+                    $scope.myData.fromServer = data.main.temp + " C " + data.sys.country;
+                });
+                responsePromise.error(function(data, status, headers, config) {
+                    alert("AJAX failed!");
+                });
+            }
+        });
