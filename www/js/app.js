598ffef90c8d9ff6faa8e7722dea4bb1fd2761a8
Mehdi Ali <mmehdi@blackberry.com>
2014-05-27 08:55:20 -0400

    feature: added ui-router interface, separated the views using ui-router

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,6 @@
-angular.module("app", [] )
-.controller("locationCtrl", function ($scope, $http) {
+var app = angular.module("app", ['ui.router'] );
+
+app.controller("locationCtrl", function ($scope, $http) {
     $scope.myData = {};
     $scope.myData.doClick = function(item, event) {
         var baseUrl = "http://api.openweathermap.org/data/2.5/weather?units=metric&q=";
@@ -14,3 +15,32 @@ angular.module("app", [] )
         });
     }
 });
+
+app.config(['$urlRouterProvider', '$stateProvider', 
+            function($urlRouterProvider, $stateProvider) {
+                $urlRouterProvider.otherwise('/');
+                $stateProvider.state("menu", {
+                    url: "/",
+                    views: {
+                        "menu": {
+                            templateUrl: "templates/menu.html"
+                        },
+                        "content":{templateUrl: "templates/weather.html"}
+                    }
+                });
+                $stateProvider.state("clock", {
+                    url: "/clock",
+                    views: {
+                        "menu": {
+                            templateUrl: "templates/menu.html"
+                        },
+                        "content": {
+                            templateUrl: "templates/clock.html",
+                            controller: function ($scope) {
+                                $scope.time = "12 O'Clock";
+                            }
+                        }
+                    }
+                    
+                });
+           }]);
